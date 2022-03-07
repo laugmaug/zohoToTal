f63c0e1cb0c6721af92a841c843c25e4c5ae9b94
Jabez Peter <58347262+laugmaug@users.noreply.github.com>
2022-03-07 11:18:20 +0200

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
         echo "";
 
         //Take-a-Lot API request block
-        header("Authorization: Key 37e7a5023327a0320c934e21275b1782048606d6ba6cec6395f5");
+        
         header("Location: https://seller-api.takealot.com/v2/offers");
 
         echo "Request successfully sent...";
