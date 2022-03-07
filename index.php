a1602024226ec49670c4e340b26b187941cb7cc7
Jabez Peter <58347262+laugmaug@users.noreply.github.com>
2022-03-07 13:35:05 +0200

    Update index.php

--- index.php
+++ index.php
@@ -8,15 +8,16 @@
 
         //Take-a-Lot API request block
         require_once("auth.php");
-        https://seller-api.takealot.com/api-docs/#/Create%20an%20offer/create_offer_by_identifier_v2
+        //https://seller-api.takealot.com/api-docs/#/Create%20an%20offer/create_offer_by_identifier_v2
         $response = file_get_contents("https://seller-api.takealot.com/v2/offers");
         echo "Request successfully sent...";
         echo "<br/>";
         
-        echo $_GET["item_id"];
+        $jsonObj = json_encode($_GET["item_id"]);
+        var_dump($jsonObj);
 
         //take-a-lot API response handler
-        var_dump($response);
+        //var_dump($response);
 
     }
 
