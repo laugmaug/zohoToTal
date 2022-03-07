324eb4e947ae17b427d27db63a66b19f8920d1a0
Jabez Peter <58347262+laugmaug@users.noreply.github.com>
2022-03-07 08:53:16 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
 
     if(isset($_GET["create_an_offer"])){
-           echo "Create an offer found";
+           echo "Create an offer found...";
+           echo "<br/>";
+           header("Key : 55a977c09b9d8a044edf4dd1ac9b94f93240d7ef1790bc93a1b95a2524f9a52ae96f3d92713e37e7a5023327a0320c934e21275b1782048606d6ba6cec6395f5");
     }
 
 ?>
