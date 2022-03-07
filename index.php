bb4e54bbd98876793281f761de24708238a9dd51
Jabez Peter <58347262+laugmaug@users.noreply.github.com>
2022-03-07 13:13:37 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,16 +2,15 @@
 
     if(isset($_GET["create_an_offer"])){
         echo "Create an offer request detected...";
-        echo "";
+        echo "<br/>";
         echo "Launching Take a Lot API request...";
-        echo "";
+        echo "<br/>";
 
         //Take-a-Lot API request block
         require_once("auth.php");
-        
         $response = file_get_contents("https://seller-api.takealot.com/v2/offers");
-
         echo "Request successfully sent...";
+        echo "<br/>";
 
         //take-a-lot API response handler
         var_dump($response);
