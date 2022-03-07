b141c2d2a24ac8be1caa956d7f461dc4cc4d0316
Jabez Peter <58347262+laugmaug@users.noreply.github.com>
2022-03-07 09:16:18 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,16 +2,16 @@
 
     if(isset($_GET["create_an_offer"])){
            echo "Create an offer request detected...";
-           echo "<br/>";
+           echo "";
            echo "Launching Take a Lot API request...";
-           echo "<br/>";
+           echo "";
         
            //Take-a-Lot API request block
            header("Authorization: Key 37e7a5023327a0320c934e21275b1782048606d6ba6cec6395f5");
            header("Location: https://seller-api.takealot.com/v2/offers");
         
            echo "Request done...";
-           echo "<br/>";
+           echo "";
     }
 
 ?>
